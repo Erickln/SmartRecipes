9de9c52080854c175c9d3e5f723169336132d89d
Abigv0606 <a01635153@itesm.mx>
2021-01-14 21:48:04 -0600

    Creación del método guardarIngrediente y una posible base de datos

--- app/src/main/java/com/example/smartrecipes/RegistroIngredientes.java
+++ app/src/main/java/com/example/smartrecipes/RegistroIngredientes.java
@@ -3,15 +3,23 @@ package com.example.smartrecipes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroIngredientes extends AppCompatActivity {
-    EditText ingrediente;
+    private EditText ingrediente;
+  //  private DataBase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_ingredientes);
 
         ingrediente = findViewById(R.id.ingredienteTexto);
+       // db = new DataBase(this);
+    }
+    public void guardarIngrediente(View v){
+       // db.guardar(ingrediente.getText().toString());
+        Toast.makeText(this, "INGREDIENTE GUARDADO", Toast.LENGTH_SHORT).show();
     }
 }
