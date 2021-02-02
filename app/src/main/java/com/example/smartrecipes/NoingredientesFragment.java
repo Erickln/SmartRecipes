8985d3f3b686b20be213a7ea354f58509b07b349
Cesar Aranda <cesararandaa@gmail.com>
2021-02-02 15:24:29 -0600

    agregado e implementado el fragmento que se muestra si no hay ingredientes

---
+++ app/src/main/java/com/example/smartrecipes/NoingredientesFragment.java
@@ -0,0 +1,36 @@
+package com.example.smartrecipes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NoingredientesFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NoingredientesFragment extends Fragment {
+
+
+    public NoingredientesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_noingredientes, container, false);
+    }
+}
